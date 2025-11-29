5cff789e7cf9e0bc47cd2636ecbd460205d67928
Clar2Verata <clar2verata@tuta.io>
2025-11-28 23:00:10 -0500

    more culling

--- modules/nixvim/plugins/copilot-lsp/default.nix
+++ modules/nixvim/plugins/copilot-lsp/default.nix
@@ -1,14 +1,4 @@
+{ config, ... }:
 {
-  plugins = {
-    copilot-lsp = {
-      # enable = config.khanelivim.ai.provider == "copilot";
-      enable = true;
 
-      settings = {
-        nes = {
-          move_count_threshold = 3;
-        };
-      };
-    };
-  };
 }
